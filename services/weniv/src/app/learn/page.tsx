8eab2db6cb12d1af0251d126ea74ba3b7253dd26
dreamfulbud <dreamfulbud@naver.com>
2024-04-15 09:06:31 +0900

    ✨ Feat: 강의 검색 필터링/강의 썸네일 추가

--- services/weniv/src/app/learn/page.tsx
+++ services/weniv/src/app/learn/page.tsx
@@ -23,18 +23,36 @@ export default function Learn() {
   const searchParams = useSearchParams();
   const [filteredData, setFilteredData] = useState(learn);
 
+  const [keyword, setKeyword] = useState<string>("");
+  const [category, setCategory] = useState<string[]>([]);
   const [teacher, setTeacher] = useState<string[]>([]);
   const [tech, setTech] = useState<string[]>([]);
   const [brand, setBrand] = useState<string[]>([]);
   const [difficulty, setDifficulty] = useState<string[]>([]);
+  const [price, setPrice] = useState<string[]>([]);
+
+  const [platform, setPlatform] = useState<string[]>([]);
+
+  const [year, setYear] = useState<number>(5);
+  const [time, setTime] = useState<number>(50);
 
   // URL 체크
   useEffect(() => {
+    const keywordParams = searchParams.get("keyword");
+    const categoryParams = searchParams.get("category");
     const teacherParams = searchParams.get("teacher");
     const techParams = searchParams.get("tech");
     const brandParams = searchParams.get("brand");
     const difficultyParams = searchParams.get("difficulty");
+    const priceParams = searchParams.get("price");
+    const platformParams = searchParams.get("platform");
 
+    const yearParams = searchParams.get("year");
+    const timeParams = searchParams.get("time");
+
+    if (categoryParams) {
+      setCategory(categoryParams.split(","));
+    }
     if (teacherParams) {
       setTeacher(teacherParams.split(","));
     }
@@ -50,11 +68,59 @@ export default function Learn() {
     if (difficultyParams) {
       setDifficulty(difficultyParams.split(","));
     }
+    if (priceParams) {
+      setPrice(priceParams.split(","));
+    }
+    if (platformParams) {
+      setPlatform(platformParams.split(","));
+    }
+    if (yearParams) {
+      setYear(Number(yearParams));
+    }
+    if (timeParams) {
+      setTime(Number(timeParams));
+    }
+    if (keywordParams) {
+      setKeyword(keywordParams);
+    }
   }, [searchParams]);
 
   // 필터링
   useEffect(() => {
     let data = learn;
+
+    if (keyword) {
+      const text = keyword.toLowerCase();
+
+      data = data.filter((item) => {
+        const isBrand = item.brand.toLowerCase().includes(text);
+        const isTitle = item.title.toLowerCase().includes(text);
+        const isDescription = item.description?.toLowerCase().includes(text);
+        const isDifficulty = item.difficulty?.toLowerCase().includes(text);
+        const isCategory = item.category?.toLowerCase().includes(text);
+
+        const isTeacher = item.teacher.join(",").includes(text);
+        const isPlatform = item.platform.join(",").includes(text);
+        const isTech = item.tech.join(",").includes(text);
+
+        return (
+          isTitle ||
+          isDescription ||
+          isBrand ||
+          isDifficulty ||
+          isCategory ||
+          isTeacher ||
+          isPlatform ||
+          isTech
+        );
+      });
+    }
+    // 기술
+    if (category.length > 0) {
+      data = data.filter((item) =>
+        category.some((q) => item.category.includes(q))
+      );
+    }
 
     // 강사
     if (teacher.length > 0) {
@@ -82,9 +148,45 @@ export default function Learn() {
       data = data.filter((item) => difficulty.includes(item.difficulty));
     }
 
+    // 가격
+    if (price.length > 0) {
+      data = data.filter((item) =>
+        price.includes(item.price === 0 ? "무료" : "유료")
+      );
+    }
+
     // 브랜드
     if (brand.length > 0) {
       data = data.filter((item) => brand.includes(item.brand));
+    }
+
+    // 기술
+    if (platform.length > 0) {
+      data = data.filter((item) =>
+        platform.some((q) => item.platform.includes(q))
+      );
+    }
+
+    // 출시기간
+    if (year === 0) {
+      const time = 1000 * 60 * 60 * 24 * 365 * 0.5;
+      data = data.filter(
+        (item) => new Date().getTime() - new Date(item.date).getTime() < time
+      );
+    } else if (year !== 5) {
+      const time = 1000 * 60 * 60 * 24 * 365 * year;
+      data = data.filter(
+        (item) => new Date().getTime() - new Date(item.date).getTime() < time
+      );
+    }
+
+    // 시간
+    if (time !== 5) {
+      data = data.filter((item) =>
+        item.time?.includes("시간")
+          ? Number(item.time?.split("시간")[0]) < time
+          : !item.time?.includes("시간")
+      );
     }
 
     setFilteredData(data);
@@ -92,6 +194,9 @@ export default function Learn() {
     // URL 파라미터 업데이트
     const queryParams = new URLSearchParams();
 
+    if (category.length > 0) {
+      queryParams.append("category", category.join(","));
+    }
     if (teacher.length > 0) {
       queryParams.append("teacher", teacher.join(","));
     }
@@ -101,14 +206,44 @@ export default function Learn() {
     if (brand.length > 0) {
       queryParams.append("brand", brand.join(","));
     }
+    if (price.length > 0) {
+      queryParams.append("price", price.join(","));
+    }
     if (difficulty.length > 0) {
       queryParams.append("difficulty", difficulty.join(","));
+    }
+    if (platform.length > 0) {
+      queryParams.append("platform", platform.join(","));
+    }
+
+    if (year) {
+      queryParams.append("year", String(year));
+    }
+    if (time) {
+      queryParams.append("time", String(time));
+    }
+    if (keyword) {
+      queryParams.append("keyword", keyword);
     }
 
     const query = queryParams.toString();
     const url = query ? `${pathname}?${query}` : pathname;
     router.push(url);
-  }, [learn, pathname, router, teacher, tech, brand, difficulty]);
+  }, [
+    learn,
+    pathname,
+    router,
+    category,
+    teacher,
+    tech,
+    brand,
+    difficulty,
+    price,
+    platform,
+    year,
+    time,
+    keyword,
+  ]);
 
   const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name, checked } = e.target;
@@ -116,6 +251,10 @@ export default function Learn() {
     let updatedArray, setUpdatedArray;
 
     switch (name) {
+      case "category":
+        updatedArray = category.slice();
+        setUpdatedArray = setCategory;
+        break;
       case "teacher":
         updatedArray = teacher.slice();
         setUpdatedArray = setTeacher;
@@ -132,51 +271,85 @@ export default function Learn() {
         updatedArray = difficulty.slice();
         setUpdatedArray = setDifficulty;
         break;
+      case "price":
+        updatedArray = price.slice();
+        setUpdatedArray = setPrice;
+        break;
+      case "platform":
+        updatedArray = platform.slice();
+        setUpdatedArray = setPlatform;
+        break;
+      case "year":
+        setYear(Number(value));
+        break;
+      case "time":
+        setTime(Number(value));
+        break;
+      case "keyword":
+        setKeyword(value);
+        break;
       default:
         return;
     }
 
-    if (checked) {
-      updatedArray.push(value);
-    } else {
-      updatedArray = updatedArray.filter((item) => item !== value);
-    }
+    if (updatedArray && setUpdatedArray) {
+      if (checked) {
+        updatedArray.push(value);
+      } else {
+        updatedArray = updatedArray.filter((item) => item !== value);
+      }
 
-    setUpdatedArray(updatedArray);
+      setUpdatedArray(updatedArray);
+    }
   };
 
   return (
-    <div>
-      <h1>검색 필터</h1>
-      <SearchFilter
-        handleQueryChange={handleQueryChange}
-        teacher={teacher}
-        tech={tech}
-        brand={brand}
-        difficulty={difficulty}
-      />
-      {filteredData && (
-        <ul className="learn">
-          {filteredData.map((item: any, index: number) => (
-            <li key={index}>
-              <Image
-                src={item.image}
-                alt=""
-                width={300}
-                height={300}
-                className="image"
-                priority={index < 8 ? true : false}
-              />
-              <p>{item.title}</p>
-              <p>{item.price}</p>
-              <p>{item.teacher}</p>
-              <p>{item.date}</p>
-              <p>{item.difficulty}</p>
-              <p>{item.time}</p>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="flex gap-10">
+      <div className="w-64 flex-shrink-0 p-4">
+        <h1>검색 필터</h1>
+        <a href="/learn">필터 초기화</a>
+
+        <SearchFilter
+          handleQueryChange={handleQueryChange}
+          keyword={keyword}
+          teacher={teacher}
+          category={category}
+          tech={tech}
+          brand={brand}
+          difficulty={difficulty}
+          price={price}
+          platform={platform}
+          year={year}
+          time={time}
+        />
+      </div>
+      <div>
+        <h2>
+          총 <strong>{filteredData.length}개</strong>의 강의가 있습니다
+        </h2>
+        {filteredData && (
+          <ul className="learn">
+            {filteredData.map((item: any, index: number) => (
+              <li key={index}>
+                <Image
+                  src={item.image}
+                  alt=""
+                  width={300}
+                  height={300}
+                  className="image"
+                  priority={index < 8 ? true : false}
+                />
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+                <p>{item.teacher}</p>
+                <p>{item.date}</p>
+                <p>{item.difficulty}</p>
+                <p>{item.time}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
--- services/weniv/src/components/SearchFilter.tsx
+++ services/weniv/src/components/SearchFilter.tsx
@@ -1,22 +1,65 @@
-import { BRAND, DIFFICULTY, TEACHER, TECH } from "@/src/constants/constants";
+import {
+  BRAND,
+  CATEGORY,
+  DIFFICULTY,
+  PLATFORM,
+  PRICE,
+  TEACHER,
+  TECH,
+} from "@/src/constants/constants";
 
 interface SearchFilterProps {
   handleQueryChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  category: string[];
   teacher: string[];
   tech: string[];
   brand: string[];
   difficulty: string[];
+  price: string[];
+  platform: string[];
+  year: number;
+  time: number;
+  keyword: string;
 }
 
 const SearchFilter = ({
   handleQueryChange,
+  category,
   teacher,
   tech,
   brand,
   difficulty,
+  price,
+  platform,
+  year,
+  time,
+  keyword,
 }: SearchFilterProps) => {
   return (
     <form>
+      <input
+        type="search"
+        placeholder="검색어"
+        name="keyword"
+        value={keyword}
+        onChange={handleQueryChange}
+        className="bg-slate-100 p-2 w-full"
+      />
+      <fieldset>
+        <legend>개발분야</legend>
+        {CATEGORY.map((name, index) => (
+          <label key={index}>
+            <input
+              type="checkbox"
+              name="category"
+              value={name}
+              onChange={handleQueryChange}
+              checked={category.includes(name)}
+            />
+            {name}
+          </label>
+        ))}
+      </fieldset>
       <fieldset>
         <legend>강사</legend>
         {TEACHER.map((name, index) => (
@@ -32,7 +75,6 @@ const SearchFilter = ({
           </label>
         ))}
       </fieldset>
-
       <fieldset>
         <legend>기술</legend>
         {TECH.map((name, index) => (
@@ -48,7 +90,6 @@ const SearchFilter = ({
           </label>
         ))}
       </fieldset>
-
       <fieldset>
         <legend>난이도</legend>
         {DIFFICULTY.map((name, index) => (
@@ -64,7 +105,21 @@ const SearchFilter = ({
           </label>
         ))}
       </fieldset>
-
+      <fieldset>
+        <legend>가격</legend>
+        {PRICE.map((name, index) => (
+          <label key={index}>
+            <input
+              type="checkbox"
+              name="price"
+              value={name}
+              onChange={handleQueryChange}
+              checked={price.includes(name)}
+            />
+            {name}
+          </label>
+        ))}
+      </fieldset>
       <fieldset>
         <legend>브랜드</legend>
         {BRAND.map((name, index) => (
@@ -79,6 +134,47 @@ const SearchFilter = ({
             {name}
           </label>
         ))}
+      </fieldset>
+      <fieldset>
+        <legend>플랫폼</legend>
+        {PLATFORM.map((name, index) => (
+          <label key={index}>
+            <input
+              type="checkbox"
+              name="platform"
+              value={name}
+              onChange={handleQueryChange}
+              checked={platform.includes(name)}
+            />
+            {name}
+          </label>
+        ))}
+      </fieldset>
+
+      <fieldset>
+        <legend>출시년도</legend>
+        <input
+          min="0"
+          max="5"
+          step="1"
+          type="range"
+          value={year}
+          name="year"
+          onChange={handleQueryChange}
+        />
+      </fieldset>
+
+      <fieldset>
+        <legend>시간</legend>
+        <input
+          min="1"
+          max="10"
+          step="1"
+          type="range"
+          value={time}
+          name="time"
+          onChange={handleQueryChange}
+        />
       </fieldset>
     </form>
   );
