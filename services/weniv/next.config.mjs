3b1fdacf58a80f324a20a48bc459ee45fb26c989
dreamfulbud <dreamfulbud@naver.com>
2024-04-24 15:00:05 +0900

    🚀 Set: 배포 설정

--- services/weniv/next.config.mjs
+++ services/weniv/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? 'https://main.weniv.co.kr' : '',
+  images: {
+    loader: 'custom',
+    loaderFile: './image-loader.js',
+  },
+};
 
 export default nextConfig;
