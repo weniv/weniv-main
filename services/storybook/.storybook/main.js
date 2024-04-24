4a0235ea069295fe3dcebf367d9110993e08cc90
dreamfulbud <dreamfulbud@naver.com>
2024-04-24 11:20:32 +0900

    🌱 Set: storybook 설정

--- services/storybook/.storybook/main.js
+++ services/storybook/.storybook/main.js
@@ -19,6 +19,7 @@ const config = {
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
     // getAbsolutePath('@chromatic-com/storybook'),
   ],
   framework: {
