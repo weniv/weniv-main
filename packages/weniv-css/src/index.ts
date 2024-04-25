8c7f1c9a4c70d8f70e62bffd306a9a3475288030
dreamfulbud <dreamfulbud@naver.com>
2024-04-25 11:30:36 +0900

    🌱 Update: 컬러코드 변수 처리

---
+++ packages/weniv-css/src/index.ts
@@ -0,0 +1,14 @@
+import './style.css';
+
+export const color = {
+  background: '#fff',
+  primary: '#2e6ff2',
+  surface: '#121314',
+  grayLv1: '#f3f5fa',
+  grayLv2: '#d9dbe0',
+  grayLv3: '#8d9299',
+  grayLv4: '#47494d',
+  error: '#ff3440',
+  warn: '#ffc533',
+  activation: '#dee8ff',
+};
